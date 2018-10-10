4b2a8f761f3798bee625b459e3d17925abc603b3
Brandon Kammerdiener <kammerdienerb@gmail.com>
2018-10-10 13:51:52 -0400

    initial commit; README

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # tree.h
+
+_tree.h_ is a header-only generic red/black implementation in C. It utilizes (abuses?) the C preprocessor to be provide a level of generality near C++ templates like the STL’s std::map.
+
+## Using ##
+To use _tree.h_, you must first `#include "tree.h"` in your source code. For every type that you wish to hold in a tree, EXACTLY ONE 'using' statement is required:
+
+```C
+/* main.c */
+#include "tree.h"
+
+use_tree(int);
+
+...
+```
+If the type to be contained is referenced with anything but a bare identifier (e.g. `char *`, `float[10]`), it must first be wrapped in a typedef:
+
+```C
+/* main.c */
+#include "tree.h"
+
+typedef char * str;
+
+use_tree(str);
+
+...
+```
+
+After that, you may use any of the following macro-like functions to create, destroy, and operate on trees and their iterators.
+
+`tree_make(key_type, value_type)`        - Allocates, initializes, and returns a new tree.
+
+`tree_make_c(key_type, value_type, cmp)` - Allocates, initializes, and returns a new tree with the comparator function `cmp`.
+
+`tree_free(t)`                           - `free()`s the tree. This does NOT deallocate tree keys or values.
+
+`tree_len(t)`                            - Returns the number of tree elements.
+
+`tree_lookup(t, key)`                    - Search the tree for `key`. Returns an iterator.
+
+`tree_insert(t, key, val)`               - Add the `key`, `val` pair to the tree or update the value associated with the key if it already exists.
+
+`tree_delete(t, key)`                    - Remove `key` and its value from the tree. Returns `1` if the key was found, `0` otherwise.
+
+`tree_begin(t)`                          - Returns an iterator to the first (ordered by key) element in the tree.
+
+`tree_last(t)`                           - Returns an iterator to the last (ordered by key) element in the tree.
+
+`tree_geq(t, key)`                       - Returns an iterator to the first key which is greater than or equal to `key`. Analogous to `std::map::lower_bound`.
+
+`tree_gtr(t, key)`                       - Returns an iterator to the first key which is greater than `key`. Analogous to `std::map::upper_bound`.
+
+`tree_it_key(it)`                        - Returns the key from iterator `it`.
+
+`tree_it_val(it)`                        - Returns the value from iterator `it`.
+
+`tree_it_next(it)`                       - Advances the iterator to the next (ordered by key) element in the tree.
+
+`tree_it_prev(it)`                       - Reverses the iterator to the previous (ordered by key) element in the tree.
+
+`tree_it_good(it)`                       - Returns `1` if the iterator is valid, `0` otherwise.
+
+`tree_traverse(t, it)`                   - Shorthand for a `for` loop that visits every tree element (ordered by key).
+
+
+By default, _tree.h_ will use the _<_ operator to compare keys. `make_tree_c` takes an additional last argument which is a function pointer that will compare keys.
+
+
+### Note ###
+_tree.h_ requires the C preprocessor's \_\_VA\_ARGS\_\_ and so requires that it is compiled in C99 mode.
+
+## Download ##
+You can clone the project with [Git](http://git-scm.com "Git") by running:
+
+    $ git clone git://github.com/kammerdienerb/tree.h
+
---
+++ tree_test.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "tree.h"
+
+use_tree(int, float);
+typedef char * str;
+use_tree(str, str);
+
+void print_tree_items(tree(int, float) t) {
+    int i = 0;
+
+    printf("tree with %llu items:\n", tree_len(t));
+
+    for (
+    tree_it(int, float) it = tree_begin(t);
+    tree_it_good(it);
+    tree_it_next(it)) {
+        printf("%d -- %d: %f\n", i++, tree_it_key(it), tree_it_val(it));
+    }
+}
+
+void print_tree(tree_node(int, float) node, int level) {
+    if (!node)
+        return;
+    int i;
+    print_tree(node->_children[1], level + 1);
+	for (i = 0; i < level; ++i)
+		printf("  ");
+	printf("%d(%d) = %f\n", node->_key, node->_parent ? node->_parent->_key : -1, node->_val);
+    print_tree(node->_children[0], level + 1);
+}
+
+int main() {
+    tree(int, float) t = tree_make(int, float);
+    tree(str, str) s_t = tree_make_c(str, str, strcmp);
+
+    for (int i = 0; i < 10; i += 1) {
+        tree_insert(t, i * i, (float)i);
+    }
+
+    print_tree(t->_root, 0);
+
+    tree_it(int, float) it = tree_lookup(t, 49);
+
+    if (tree_it_good(it)) {
+        printf("found 49: %f\n", tree_it_val(it));
+    } else {
+        printf("did not find 49\n");
+    }
+    it = tree_lookup(t, 80);
+    if (tree_it_good(it)) {
+        printf("found 80: %f\n", tree_it_val(it));
+    } else {
+        printf("did not find 80\n");
+    }
+
+    it = tree_geq(t, 49);
+    printf("geq(49) = %d\n", tree_it_key(it));
+
+    it = tree_gtr(t, 49);
+    printf("gtr(49) = %d\n", tree_it_key(it));
+
+    tree_traverse(t, it) {
+        printf("%d -- %f\n", tree_it_key(it), tree_it_val(it));
+    }
+
+    tree_it_prev(it);
+
+    printf("last: %d\n", tree_it_key(it));
+
+    for (it = tree_begin(t); tree_it_good(it); it = tree_begin(t)) {
+        printf("-----------------------\ndeleting %d\n", tree_it_key(it));
+        tree_delete(t, tree_it_key(it));
+        print_tree(t->_root, 0);
+    }
+
+    tree_insert(s_t, strdup("epoch"),    strdup("exultant"));
+    tree_insert(s_t, strdup("scarf"),    strdup("magic"));
+    tree_insert(s_t, strdup("feeling"),  strdup("general"));
+    tree_insert(s_t, strdup("fearless"), strdup("tremendous"));
+    tree_insert(s_t, strdup("dapper"),   strdup("spicy"));
+    tree_insert(s_t, strdup("helpful"),  strdup("fool"));
+    tree_insert(s_t, strdup("laugh"),    strdup("tan"));
+    tree_insert(s_t, strdup("toe"),      strdup("tacky"));
+    tree_insert(s_t, strdup("flight"),   strdup("eager"));
+    tree_insert(s_t, strdup("oval"),     strdup("income"));
+    tree_insert(s_t, strdup("jitter"),   strdup("weigh"));
+    tree_insert(s_t, strdup("yam"),      strdup("small"));
+
+    if (tree_delete(s_t, "doesn't exist"))
+        printf("how did we delete a key that doesn't exist?\n");
+
+    char * k, *v;
+    tree_it(str, str) s_it;
+
+    s_it = tree_lookup(s_t, "oval");
+    k = tree_it_key(s_it);
+    v = tree_it_val(s_it);
+
+    if (tree_delete(s_t, "oval")) {
+        printf("deleted 'oval'\n");
+        free(k);
+        free(v);
+    }
+
+    printf("\n");
+
+    int max_len = 0;
+    tree_traverse(s_t, s_it) {
+        int l = strlen(tree_it_key(s_it));
+        if (l > max_len)
+            max_len = l;
+    }
+    tree_traverse(s_t, s_it) {
+        printf("%*s %s\n", -max_len, tree_it_key(s_it), tree_it_val(s_it));
+        free(tree_it_key(s_it));
+        free(tree_it_val(s_it));
+    }
+
+    tree_free(s_t);
+    tree_free(t);
+
+    return 0; 
+}
